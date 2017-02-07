59640f518b2c8f535d28987f1ef0f3a1617d46e5
Stephen Bourget <steve.bourget@sau19.org>
2017-02-07 16:07:42 -0500

    MDL-12689 auth_pam: Migrate to admin settings API

--- auth/pam/db/upgrade.php
+++ auth/pam/db/upgrade.php
@@ -15,15 +15,30 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version information
+ * PAM authentication plugin upgrade code
  *
  * @package    auth_pam
- * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
+ * @copyright  2017 Stephen Bourget
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017020700;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2016112900;        // Requires this Moodle version
-$plugin->component = 'auth_pam';       // Full name of the plugin (used for diagnostics)
+/**
+ * @param int $oldversion the version we are upgrading from
+ * @return bool result
+ */
+function xmldb_auth_pam_upgrade($oldversion) {
+    global $CFG, $DB;
+
+    // Automatically generated Moodle v3.2.0 release upgrade line.
+    // Put any upgrade step following this.
+
+    if ($oldversion < 2017020700) {
+        // Convert info in config plugins from auth/pam to auth_pam
+        $DB->set_field('config_plugins', 'plugin', 'auth_pam', array('plugin' => 'auth/pam'));
+        upgrade_plugin_savepoint(true, 2017020700, 'auth', 'pam');
+    }
+    
+    return true;
+}
