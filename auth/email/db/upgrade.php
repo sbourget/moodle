2f1ae14cf39fc7f0a74539c8eaac417969d8a219
Stephen Bourget <steve.bourget@sau19.org>
2017-02-07 13:09:31 -0500

    MDL-12689 auth_email: Migrate to admin settings API

--- auth/email/db/upgrade.php
+++ auth/email/db/upgrade.php
@@ -15,15 +15,31 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version details
+ * No authentication plugin upgrade code
  *
  * @package    auth_email
- * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
+ * @copyright  2017 Stephen Bourget
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die;
+defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017020700;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2016112900;        // Requires this Moodle version
-$plugin->component = 'auth_email';      // Full name of the plugin (used for diagnostics)
+/**
+ * @param int $oldversion the version we are upgrading from
+ * @return bool result
+ */
+function xmldb_auth_email_upgrade($oldversion) {
+    global $CFG, $DB;
+
+    // Automatically generated Moodle v3.2.0 release upgrade line.
+    // Put any upgrade step following this.
+
+    if ($oldversion < 2017020700) {
+        // Convert info in config plugins from auth/email to auth_email
+        $DB->set_field('config_plugins', 'plugin', 'auth_email', array('plugin' => 'auth/email'));
+        upgrade_plugin_savepoint(true, 2017020700, 'auth', 'email');
+    }
+    
+    return true;
+}
+
