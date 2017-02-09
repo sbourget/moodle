de42feb7f5d997129e57aa6a4ba592e541fb46e8
Stephen Bourget <steve.bourget@sau19.org>
2017-02-08 19:51:09 -0500

    MDL-12689 auth_db: Migrate to admin settings API

--- auth/db/db/upgrade.php
+++ auth/db/db/upgrade.php
@@ -15,15 +15,30 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version details
+ * DB authentication plugin upgrade code
  *
  * @package    auth_db
- * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
+ * @copyright  2017 Stephen Bourget
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017020700;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2016112900;        // Requires this Moodle version
-$plugin->component = 'auth_db';         // Full name of the plugin (used for diagnostics)
+/**
+ * @param int $oldversion the version we are upgrading from
+ * @return bool result
+ */
+function xmldb_auth_db_upgrade($oldversion) {
+    global $CFG, $DB;
+
+    // Automatically generated Moodle v3.2.0 release upgrade line.
+    // Put any upgrade step following this.
+
+    if ($oldversion < 2017020700) {
+        // Convert info in config plugins from auth/db to auth_db
+        $DB->set_field('config_plugins', 'plugin', 'auth_db', array('plugin' => 'auth/db'));
+        upgrade_plugin_savepoint(true, 2017020700, 'auth', 'db');
+    }
+    
+    return true;
+}
