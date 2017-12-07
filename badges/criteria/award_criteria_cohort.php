688c67f18a4eb1510d2fcabaed402eb8b6e35f4f
Stephen Bourget <steve.bourget@sau19.org>
2017-12-06 20:58:39 -0500

    MDL-55491 badges: Add cohort as badge criteria

--- badges/criteria/award_criteria_cohort.php
+++ badges/criteria/award_criteria_cohort.php
@@ -31,6 +31,11 @@ require_once($CFG->dirroot.'/cohort/lib.php');
 /**
  * Badge award criteria -- award on cohort membership
  *
+ * @package    core
+ * @subpackage badges
+ * @copyright  2016 onwards Catalyst IT {@link https://www.catalyst.net.nz/}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author     Eugene Venter <eugene@catalyst.net.nz>
  */
 class award_criteria_cohort extends award_criteria {
 
@@ -42,7 +47,7 @@ class award_criteria_cohort extends award_criteria {
 
     /**
      * Get criteria details for displaying to users
-     *
+     * @param string $short Print short version of criteria
      * @return string
      */
     public function get_details($short = '') {
@@ -77,7 +82,7 @@ class award_criteria_cohort extends award_criteria {
         $mform->addElement('header', 'first_header', $this->get_title());
         $mform->addHelpButton('first_header', 'criteria_' . $this->criteriatype, 'badges');
 
-        // Get cohorts
+        // Get cohorts.
         $cohorts = $DB->get_records_menu('cohort', array(), 'name ASC', 'id, name');
         if (!empty($cohorts)) {
             $select = array();
@@ -122,7 +127,7 @@ class award_criteria_cohort extends award_criteria {
     /**
      * Save criteria records
      *
-     * @param $params criteria params
+     * @param array $params Values from the form or any other array.
      */
     public function save($params = array()) {
         $cohorts = $params['cohort_cohorts'];
@@ -137,7 +142,11 @@ class award_criteria_cohort extends award_criteria {
     /**
      * Review this criteria and decide if it has been completed
      *
-     * @return bool Whether criteria is complete
+     * @param int $userid User whose criteria completion needs to be reviewed.
+     * @param bool $filtered An additional parameter indicating that user list
+     *        has been reduced and some expensive checks can be skipped.
+     *
+     * @return bool Whether criteria is complete.
      */
     public function review($userid, $filtered = false) {
         global $DB;
@@ -197,9 +206,43 @@ class award_criteria_cohort extends award_criteria {
         return array(true, '');
     }
 
+    /**
+     * Returns array with sql code and parameters returning all ids
+     * of users who meet this particular criterion.
+     *
+     * @return array list($join, $where, $params)
+     */
     public function get_completed_criteria_sql() {
-// TODO;
+        $join = '';
+        $where = '';
+        $params = array();
 
-        return array($join, $where, $params);
+        if ($this->method == BADGE_CRITERIA_AGGREGATION_ANY) {
+            // User is a member of ANY of the specified cohorts.
+            $join = " LEFT JOIN {cohort_members} cm ON cm.userid = u.id";
+            $where = "AND (";
+            $i = 0;
+            foreach ($this->params as $param) {
+                if ($i == 0) {
+                    $where .= ' cm.cohortid = :cohortid'.$i;
+                } else {
+                    $where .= ' OR cm.cohortid = :cohortid'.$i;
+                }
+                $params['cohortid'.$i] = $param['cohort'];
+                $i++;
+            }
+            $where .= ") ";
+            return array($join, $where, $params);
+        } else {
+            // User is a member of ALL of the specified cohorts.
+            $join = " LEFT JOIN {cohort_members} cm ON cm.userid = u.id";
+            $i = 0;
+            foreach ($this->params as $param) {
+                $i++;
+                $where = ' AND cm.cohortid = :cohortid'.$i;
+                $params['cohortid'.$i] = $param['cohort'];
+            }
+            return array($join, $where, $params);
+        }
     }
 }
