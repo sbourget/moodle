af463171f065335d1a34d64d10549724ff667591
Jonathan Champ <jonathan.r.champ@gmail.com>
2026-07-01 09:32:47 +0200

    MDL-89058 lib: check contact earlier to simplify code

--- public/lib/enrollib.php
+++ public/lib/enrollib.php
@@ -3648,6 +3648,15 @@ abstract class enrol_plugin {
         $course = get_course($instance->courseid);
         $context = context_course::instance($course->id);
 
+        $contact = $this->get_welcome_message_contact(
+            sendoption: $sendoption,
+            context: $context,
+        );
+        if (!$contact) {
+            // Cannot find the contact to send the message from.
+            return;
+        }
+
         // Fallback to the instance role ID if parameter not specified.
         $courseroleid = $roleid ?: $instance->roleid;
         $courserole = $DB->get_record('role', ['id' => $courseroleid]);
@@ -3706,16 +3715,6 @@ abstract class enrol_plugin {
                 ['context' => $context, 'escape' => false]
             );
             $messagehtml = text_to_html($messagetext, null, false, true);
-        }
-
-        $contact = $this->get_welcome_message_contact(
-            sendoption: $sendoption,
-            context: $context,
-        );
-        if (!$contact) {
-            force_current_language($oldforcelang);
-            // Cannot find the contact to send the message from.
-            return;
         }
 
         $message = new \core\message\message();
